47fe167c946039bdaf6ae04b37cb8a502fcb88d4
Wxz12333 <876979426@qq.com>
2025-03-20 16:03:06 +0800

    Update hiv_model_student.py

--- src/hiv_model_student.py
+++ src/hiv_model_student.py
@@ -5,7 +5,7 @@ class HIVModel:
     def __init__(self, A, alpha, B, beta):
         # TODO: 初始化模型参数
         pass
-
+ 
     def viral_load(self, time):
         # TODO: 计算病毒载量
         return np.zeros_like(time)
